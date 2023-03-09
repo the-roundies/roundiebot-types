469947265204681fc434a56fc2a2929ee6c3e886
SlowlyPoorly <slowlypoorly@gmail.com>
2023-03-09 08:27:02 -0500

    feat: add enums

---
+++ types/enums.ts
@@ -0,0 +1,17 @@
+export enum EventType {
+    userJailed = 0,
+    userHospitalized = 1,
+    userSpooked = 3,
+    juryDuty = 4,
+    foxCopDisabled = 5,
+    foxLawyerDisabled = 6,
+    excitedBearDisabled = 7,
+    userOnTrial = 8,
+    userOnHunt = 9,
+    transactionConfirmation = 10,
+    userWalletSnapshotRequest = 12,
+    deceptivelyCuteGatorDisabled = 13,
+    userPhantomFoxed = 14,
+    userInsane = 15,
+    userLaidTrapInChannel = 16,
+}
